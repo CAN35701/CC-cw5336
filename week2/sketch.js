e3738c1e2f945744514fb61168bf732eb84a2c01
CAN35701 <cw5336@nyu.edu>
2026-09-11 16:19:22 -0400

    Update sketch.js

--- week2/sketch.js
+++ week2/sketch.js
@@ -1,32 +1,8 @@
-
-let r; //for storing my randm number
-//scale,rotate,translation
-
 function setup() {
-  createCanvas(400,400);
-    
-    r = random(100,255);
-    r2 = random(0,360);
-    console.log(r);
+  createCanvas(windowWidth, windowHeight);
+  
 }
-
-
 
 function draw() {
-  background(r,0,0);
-
-  push();//isolate
-  translate(width/2,height/2);//shift origin
-  rectMode(CENTER);
-  rotate(r2);//0-360
-  rect(0,0,r);
-  pop();//isolate
-
-  ellipse(300,300,150);
+  background("red");
 }
-
-function mousePressed() {
-  r = random(100,255);
-  r2 = random(0,360);
-}
-
